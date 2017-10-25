081aa69e44c5cd1ab48d435b1a98764ac297715a
Stephen Whitmore <sww@eight45.net>
2017-10-24 23:48:26 -0700

    hpad init

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,6 +3,8 @@
 var fs = require('fs')
 var path = require('path')
 var args = require('minimist')(process.argv)
+var hstring = require('hyper-string')
+var level = require('level')
 
 if (args.h || args.help) {
   return exit(0)
@@ -16,6 +18,22 @@ var subcommand = args._[2]
 var file = args._[3]
 
 if (subcommand === 'init') {
+  if (!fs.existsSync(file)) {
+    return exit(1)
+  }
+  var filename = '.hpad-' + path.basename(file)
+  var dirname = path.dirname(file)
+  var dbpath = path.join(dirname, filename)
+  if (fs.existsSync(dbpath)) {
+    console.error('ERROR:', file, 'is already backed by a hyperpad.')
+    return process.exit(1)
+  }
+  var str = hstring(level(dbpath))
+  var txt = fs.readFileSync(file, 'utf8')
+  str.insert(null, txt, function (err) {
+    if (err) throw err
+    console.log('Now backing', file, 'with a hyperpad.')
+  })
 } else if (subcommand === 'update') {
 } else if (subcommand === 'sync') {
 } else {
@@ -29,3 +47,4 @@ function exit (code) {
     process.exit(code)
   })
 }
+
