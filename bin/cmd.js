e3631068a3d578560ac314cdd1538fdbcb4c3f1d
Stephen Whitmore <sww@eight45.net>
2017-10-25 00:22:46 -0700

    add subcommands "update" and "print"

--- bin/cmd.js
+++ bin/cmd.js
@@ -36,7 +36,64 @@ if (subcommand === 'init') {
     console.log('Now backing', file, 'with a hyperpad.')
   })
 } else if (subcommand === 'update') {
+  if (!fs.existsSync(file)) {
+    return exit(1)
+  }
+  var filename = '.hpad-' + path.basename(file)
+  var dirname = path.dirname(file)
+  var dbpath = path.join(dirname, filename)
+  if (!fs.existsSync(dbpath)) {
+    console.error('ERROR:', file, 'isnt backed by a hyperpad.')
+    return process.exit(1)
+  }
+  var str = hstring(level(dbpath))
+  var txt = fs.readFileSync(file, 'utf8')
+  str.chars(function (err, chars) {
+    if (err) throw err
+    var htxt = chars.map(function (ch) { return ch.chr }).join('')
+    var changes = diff(htxt, txt)
+
+    var pos = 0
+    function processChange () {
+      if (!changes.length) {
+        console.error('done')
+        return
+      }
+      var change = changes.shift()
+      console.log('change', change, pos)
+      if (!change.added && !change.removed) {
+        pos += change.value.length
+        process.nextTick(processChange)
+      } else if (change.added) {
+        console.log('inserting', change.value, 'at', chars[pos].pos)
+        var at = pos > 0 ? chars[pos-1].pos : null
+        str.insert(at, change.value, processChange)
+      } else if (change.removed) {
+        console.log('deleting', change.value.length, 'at', chars[pos].pos)
+        str.delete(chars[pos].pos, change.value.length, processChange)
+        pos += change.value.length
+      } else {
+        throw new Error('this shouldn\'t happen')
+      }
+    }
+    processChange()
+  })
 } else if (subcommand === 'sync') {
+} else if (subcommand === 'print') {
+  if (!fs.existsSync(file)) {
+    return exit(1)
+  }
+  var filename = '.hpad-' + path.basename(file)
+  var dirname = path.dirname(file)
+  var dbpath = path.join(dirname, filename)
+  if (!fs.existsSync(dbpath)) {
+    console.error('ERROR:', file, 'isnt backed by a hyperpad.')
+    return process.exit(1)
+  }
+  var str = hstring(level(dbpath))
+  str.text(function (err, txt) {
+    console.log(txt)
+  })
 } else {
   exit(1)
 }
